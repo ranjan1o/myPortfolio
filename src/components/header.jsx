a4009792eca7f9aadc231e21126420361f27017c
ranjan1o <84707304+ranjan1o@users.noreply.github.com>
2021-09-04 17:17:01 +0530

    Add files via upload

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import "./Style.css"
 import profilepic from "../images/profilepic.jpg"
 import resume from "../Resume/resume.pdf"
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 const A1 = styled.div`
 
 margin-top: 80px;
@@ -14,6 +16,15 @@ border-radius: 50%;
 
 }
 @media screen and (max-width: 600px) {
+  .iou{
+    width: 80%;
+    margin: auto;
+
+  }
+  .grid1{
+    display: grid;
+    grid-template-columns: 100%;
+  }
   .display{
 display: none;
 }
@@ -31,15 +42,26 @@ display: none;
     background: linear-gradient(
 270deg
 ,rgb(19,173,199) 0%,rgb(148,93,214) 100%);
-}`
+}
+`
 
 export function Header() {
+  const textRef = useRef();
+   useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Full-Stack Web Developer","MERN Full Stack Developer"],
+    });
+  }, []);
     return <A1 >
-        <div className="grid1 grid2">
-        <div className="">
+        <div className="grid1">
+        <div className="iou">
         <h1 className="smt">Welcome To</h1>
 <h1 className="smt">My Personal Portfolio</h1>
-                <h1 className="colorp">Hi, I am Ranjan Giri</h1>
+          <h1 className="colorp">Hi, I am Ranjan Giri</h1>
+          <h3><span ref={textRef} className="colorp"></span></h3>
                 <div><button className="cvbtn"><a href={resume} download style={{color:"white"}} >Download My CV</a></button></div>
         </div>
         
