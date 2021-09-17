0a30fefc2282e8999624032f65d26b4934679da8
Ranjan Giri <84707304+ranjan1o@users.noreply.github.com>
2021-09-17 20:25:49 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,8 @@ const A = styled.div`
 
 padding-top:20px;
 width: 100%;
-display: grid;
-grid-template-columns: 25% 50% 25%;
-text-align: center;
+display: flex;
+
 font-size: x-large;
 
 
@@ -40,11 +39,12 @@ font-size: x-large;
   margin-left: 10px;
   background-color:darkblue;
   font-size: xx-large;
- font-family:Verdana, Geneva, Tahoma, sans-serif;
+ font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 @media screen and (max-width:600px) {
  .navres{
    display: none;
+  
  }
 }
 .ict{
@@ -53,24 +53,31 @@ font-size: x-large;
   padding:2px 2px 0px 2px;
 }
 
+.ghy{
+  width:80px;
+}
 `
 export function Navbar() {
-    return <A>
-      <div className="flex">
+  return <A>
+      <div style={{width:"100%"}}>
+      <div className="flex ghy">
         <div className="colorp">R</div>
       </div>
-      <div className="navres">
-        <div className="flex bordernone"><button className="bbnone textStyling" onClick={() => window.scrollTo(0, 1800)}> Project</button>
+      <div>
+      <div style={{float:"right",display:"flex"}}>
+        <div className="flex bordernone navres" ><button className="bbnone textStyling" onClick={() => window.scrollTo(0, 1800)}> Project</button>
           <button className="bbnone textStyling" onClick={() => window.scrollTo(0, 750)}>Skill</button>
           <button className="bbnone textStyling" onClick={() => window.scrollTo(0, 300)}> About</button>
-        <button className="bbnone textStyling" onClick={() => window.scrollTo(0, 2700)}>Contact</button></div>
-      </div>
-      <div className="flex">
-        <div style={{float:"right",marginRight:"10px"}}>
+        <button className="bbnone textStyling" onClick={() => window.scrollTo(0, 2700)}>Contact</button>
+          </div>
+          <div>
         <button className="bbnone"><a href="https://github.com/ranjan1o" target="_blank" rel="noreferrer"><GitHubIcon className="iconheader"/></a></button>
           <button className="bbnone"><a href="https://www.linkedin.com/in/ranjan-giri-0097521a7/" target="_blank" rel="noreferrer"><LinkedInIcon className="iconheader"/></a></button>
           <button className="bbnone"><TwitterIcon className="iconheader"/></button>
-        </div></div>
+          </div>
+        </div>
+        </div>
+      </div>
    
         </A>
 }
